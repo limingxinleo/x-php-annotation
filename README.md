b91b7a88f49bedc613e2d17f4b1fbb82f63f849b
李铭昕 <715557344@qq.com>
2018-05-17 16:21:31 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,13 @@
+# X-Annotation
+
+[![Build Status](https://travis-ci.org/limingxinleo/x-php-annotation.svg?branch=master)](https://travis-ci.org/limingxinleo/x-php-annotation)
+
+## 安装
+~~~
+composer require limingxinleo/x-php-annotation
+~~~
+
+## 使用
+~~~php
+
+~~~
---
+++ tests/App/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | IndexController.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace Tests\App;
+
+class IndexController
+{
+    /**
+     * @desc
+     * @author limx
+     * @Message('首页')
+     */
+    public function index()
+    {
+
+    }
+}
